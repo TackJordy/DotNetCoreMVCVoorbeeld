6aeb0efb7fdb8de860902ad7df22657aaaa9de2f
dragonname <jordytack@hotmail.com>
2018-02-19 11:28:49 +0100

    with hello name

--- DotNetCoreMVCVoorbeeld/Controllers/HomeController.cs
+++ DotNetCoreMVCVoorbeeld/Controllers/HomeController.cs
@@ -10,28 +10,11 @@ namespace DotNetCoreMVCVoorbeeld.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public string Index(string name)
         {
-            return View();
+            return $"Hallo {name}";
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
