8480e8a6c2f9daab6d58c8ca467765f18eaf58b9
dragonname <jordytack@hotmail.com>
2018-02-19 11:55:31 +0100

    With Groenten

--- DotNetCoreMVCVoorbeeld/Controllers/HomeController.cs
+++ DotNetCoreMVCVoorbeeld/Controllers/HomeController.cs
@@ -10,11 +10,24 @@ namespace DotNetCoreMVCVoorbeeld.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index(string name)
+        private string[] groenten = {"Rode kool","Spruitjes","Wortel","Spinazie" };
+        public ViewResult Index(string name)
         {
-            return $"Hallo {name}";
+            ViewBag.Groet = DateTime.Now.Hour < 12 ? "Goeiemorgen" : "Goeiemiddag";
+            return View();
         }
 
-       
+        public ViewResult Groenten(string zoekGroente)
+        {
+            ViewBag.Groenten = groenten;
+            if (!string.IsNullOrEmpty(zoekGroente))
+            {
+                ViewBag.Zoekresultaat = $"de gezochte groente is de {Array.IndexOf(groenten, zoekGroente) + 1}e uit de lijst";
+            }
+            
+            return View();
+        }
+
+
     }
 }
